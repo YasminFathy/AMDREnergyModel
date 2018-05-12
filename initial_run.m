e1b6c812c853a7646f09327bb72ffa1a427c97cf
Yasmin Fathy <Fathy.Yasmin@gmail.com>
2018-05-12 21:40:10 +0100

    Create initial_run.m

---
+++ initial_run.m
@@ -0,0 +1,15 @@
+addpath('AM-DR');
+addpath('baseline');
+
+addpath('dataset');
+addpath('dataset/case_I');
+addpath('dataset/case_II');
+
+
+addpath('LEACH');
+
+addpath('plot_results');
+addpath('plot_results/case_I');
+addpath('plot_results/case_II');
+
+
